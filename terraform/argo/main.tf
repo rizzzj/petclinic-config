9bc2a5303dc27c7030aed2e5868934297a62f093
Riz Josel Urian <rurian@apple.com>
2026-01-08 20:10:16 +0800

    migrate kube manifest to terraform

--- terraform/argo/main.tf
+++ terraform/argo/main.tf
@@ -14,3 +14,78 @@ module "petclinic" {
   use_helm              = true
   helm_values           = ["values.yaml"]
 }
+
+module "elasticsearch" {
+  source = "./modules"
+
+  providers = {
+    argocd     = argocd
+    kubernetes = kubernetes
+  }
+
+  name                  = "elasticsearch"
+  path                  = "app"
+  repo_url              = "https://github.com/rizjosel/petclinic-config.git"
+  branch                = "stable"
+  destination_namespace = "logging"
+}
+
+module "kibana" {
+  source = "./modules"
+
+  providers = {
+    argocd     = argocd
+    kubernetes = kubernetes
+  }
+
+  name                  = "kibana"
+  path                  = "app"
+  repo_url              = "https://github.com/rizjosel/petclinic-config.git"
+  branch                = "stable"
+  destination_namespace = "logging"
+}
+
+module "filebeat" {
+  source = "./modules"
+
+  providers = {
+    argocd     = argocd
+    kubernetes = kubernetes
+  }
+
+  name                  = "filebeat"
+  path                  = "app"
+  repo_url              = "https://github.com/rizjosel/petclinic-config.git"
+  branch                = "stable"
+  destination_namespace = "logging"
+}
+
+module "grafana" {
+  source = "./modules"
+
+  providers = {
+    argocd     = argocd
+    kubernetes = kubernetes
+  }
+
+  name                  = "grafana"
+  path                  = "app"
+  repo_url              = "https://github.com/rizjosel/petclinic-config.git"
+  branch                = "stable"
+  destination_namespace = "grafana"
+}
+
+module "prometheus" {
+  source = "./modules"
+
+  providers = {
+    argocd     = argocd
+    kubernetes = kubernetes
+  }
+
+  name                  = "prometheus"
+  path                  = "app"
+  repo_url              = "https://github.com/rizjosel/petclinic-config.git"
+  branch                = "stable"
+  destination_namespace = "prometheus"
+}
