cd47438db510ab15b4285c95a7b3e7eed3890a3b
Riz Josel Urian <rurian@apple.com>
2026-01-16 20:57:53 +0800

    remove app terraform

--- terraform/argo/main.tf
+++ terraform/argo/main.tf
@@ -1,19 +1,19 @@
-module "petclinic" {
-  source = "./modules"
+#module "petclinic" {
+#  source = "./modules"
 
-  providers = {
-    argocd     = argocd
-    kubernetes = kubernetes
-  }
+#  providers = {
+#    argocd     = argocd
+#    kubernetes = kubernetes
+#  }
 
-  name                  = "petclinic"
-  path                  = "app"
-  repo_url              = "https://github.com/rizjosel/petclinic-config.git"
-  branch                = "HEAD"
-  destination_namespace = "myapp"
-  use_helm              = true
-  helm_values           = ["values.yaml"]
-}
+#  name                  = "petclinic"
+#  path                  = "app"
+#  repo_url              = "https://github.com/rizjosel/petclinic-config.git"
+#  branch                = "HEAD"
+#  destination_namespace = "myapp"
+#  use_helm              = true
+#  helm_values           = ["values.yaml"]
+#}
 
 module "elasticsearch" {
   source = "./modules"
