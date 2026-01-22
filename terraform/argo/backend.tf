3af5e7db00e263692f62fff44b570a59e73f68d8
Riz Josel Urian <rurian@apple.com>
2026-01-22 21:12:07 +0800

    enable backend s3

--- terraform/argo/backend.tf
+++ terraform/argo/backend.tf
@@ -1,6 +1,19 @@
+resource "aws_s3_bucket" "mybucket"{
+  bucket = "s3statebackend100524"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+}
 terraform {
   backend "s3" {
-    bucket         = "mybucket"
+    bucket         = "s3statebackend100524"
     key            = "argo/app/terraform.tfstate"
     region         = "ap-southeast-1"
     use_lockfile   = true
